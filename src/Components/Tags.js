e3995ed4ae43071f60562976b77ccb5ce80729a5
saifbinabdulkarim <saifbinabdukarim.sk@gmail.com>
2019-06-15 20:03:55 +0500

    UI chamges

--- src/Components/Tags.js
+++ src/Components/Tags.js
@@ -1,18 +1,28 @@
 import React from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 
 
-export default function Tag() {
+export default class Tag extends React.Component {
+    componentDidMount(){
+        AOS.init({
+          duration : 2000
+        })
+      }
+     render(){ 
     return ( 
         <div >
-            <h1 style = {
+            <h1 data-aos='fade-up' style = {
                 {
-                    paddingTop: "2in",
+                    paddingTop: "1.3in",
+                    paddingLeft:"1.8in",
                     color: "snow",
                     fontFamily: "sanserif",
                     fontSize: "50px"
                 }
-            } > Your Choice, Our Service: ) </h1> 
+            } > Your Choice, Our Service :) </h1> 
             </div>
-)
+        )
+    }
 };
